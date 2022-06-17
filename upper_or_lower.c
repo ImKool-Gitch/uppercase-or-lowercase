bf0d39f6f3cadbb1447209630f79d80646b26aea
ImKool-Gitch <71771827+ImKool-Gitch@users.noreply.github.com>
2022-06-17 20:20:59 +0200

    Create upper_or_lower.c

---
+++ upper_or_lower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void letter()
+{
+    printf("Made by ImKool-Gitch\n");
+    char character;
+    printf("Enter a character: ");
+    scanf("%c", &character);
+    if (character >= 65 && character < 91)
+    {
+        printf("character is uppercase");
+    }
+    else if (character >= 97 && character < 123)
+    {
+        printf("character is lowercase");
+    }
+    else
+    {
+        printf("no symbols allowed only alphabet !");
+    }
+}
